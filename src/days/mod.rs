e33e2947a8bea19255533077ed71c287a06c8918
Andrew Bowers <andrew.bowers@emberex.com>
2017-12-18 17:22:06 -0800

    Stubbed out Day 8 and refactored main again.

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,7 +1,19 @@
-pub mod day_1;
-pub mod day_2;
-pub mod day_3;
-pub mod day_4;
-pub mod day_5;
-pub mod day_6;
-pub mod day_7;
+mod day_1;
+mod day_2;
+mod day_3;
+mod day_4;
+mod day_5;
+mod day_6;
+mod day_7;
+mod day_8;
+
+pub const DAYS: [fn(); 8] = [
+    day_1::main,
+    day_2::main,
+    day_3::main,
+    day_4::main,
+    day_5::main,
+    day_6::main,
+    day_7::main,
+    day_8::main
+];
